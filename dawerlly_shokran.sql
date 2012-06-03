4a369fcec3ef56398ace93d582795a2e4c7e0dca
mo3tssem <mo3tssem@facebook.com>
2012-06-03 17:24:05 +0300

    upload db file

---
+++ dawerlly_shokran.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.18 
+MySQL - 5.0.51b-community-nt-log : Database - dawerllyshokran
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dawerllyshokran` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `dawerllyshokran`;
+
+/*Table structure for table `ds_stable` */
+
+DROP TABLE IF EXISTS `ds_stable`;
+
+CREATE TABLE `ds_stable` (
+  `st_id` int(11) NOT NULL auto_increment,
+  `st_title` varchar(255) collate utf8_unicode_ci default NULL,
+  `st_image` varchar(255) collate utf8_unicode_ci default NULL,
+  `st_sub_desc` varchar(255) collate utf8_unicode_ci default NULL,
+  `st_desc` text collate utf8_unicode_ci,
+  `st_type` enum('home','land','store') collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`st_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `ds_stable` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
